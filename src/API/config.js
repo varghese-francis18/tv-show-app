8f185c77056d88d6a2bb17b7b67b8c16cfa689f4
Varghese Francis <varghesefrancis253@gmail.com>
2023-12-19 08:09:39 +0530

    api key secured

--- src/API/config.js
+++ src/API/config.js
@@ -1,6 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "?api_key=04c7c03d0344439e16f1f2666546d202";
 const BACKDROP_PATH = "https://image.tmdb.org/t/p/original";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
-export { BASE_URL, API_KEY, BACKDROP_PATH, SMALL_IMG_COVER_BASE_URL };
+export { BASE_URL, BACKDROP_PATH, SMALL_IMG_COVER_BASE_URL };
--- src/tv-show.js
+++ src/tv-show.js
@@ -1,22 +1,24 @@
 import axios from "axios";
-import { BASE_URL, API_KEY } from "./API/config";
+import { BASE_URL } from "./API/config";
 
 export class TVShowAPI {
   static async fetchPopulars() {
-    const response = await axios.get(`${BASE_URL}tv/popular${API_KEY}`);
+    const response = await axios.get(
+      `${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY}`
+    );
     return response.data.results;
   }
 
   static async fetchRecommendations(tvShowId) {
     const response = await axios.get(
-      `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY}`
+      `${BASE_URL}tv/${tvShowId}/recommendations?api_key=${process.env.REACT_APP_API_KEY}`
     );
     return response.data.results;
   }
 
   static async fetchByTitle(title) {
     const response = await axios.get(
-      `${BASE_URL}search/tv${API_KEY}&query=${title}`
+      `${BASE_URL}search/tv${process.env.REACT_APP_API_KEY}&query=?api_key=${title}`
     );
     return response.data.results;
   }
